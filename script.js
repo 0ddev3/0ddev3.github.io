e62b59ce3a2a08d463bd022872d63af1c4963e6c
Yvette Boyd <ev3boyd@gmail.com>
2025-09-26 00:48:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
 }
+const nameText = "Yvette Boyd";
+let index = 0;
+
+function typeName() {
+  if (index < nameText.length) {
+    document.getElementById("typed-name").textContent += nameText.charAt(index);
+    index++;
+    setTimeout(typeName, 150);
+  }
+}
+
+window.onload = typeName;
