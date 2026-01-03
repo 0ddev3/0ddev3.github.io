3f4779f81da93e509b7cc13c085c04e8fcab054a
Yvette Boyd <ev3boyd@gmail.com>
2026-01-02 18:11:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // script.js
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
-const nameText = "Yvette Boyd";
-let index = 0;
-
-function typeName() {
-  if (index < nameText.length) {
-    document.getElementById("typed-name").textContent += nameText.charAt(index);
-    index++;
-    setTimeout(typeName, 150);
-  }
-}
-
-window.onload = typeName;
+// AOS animation initialization
+window.addEventListener("load", () => {
+  AOS.init();
+});
